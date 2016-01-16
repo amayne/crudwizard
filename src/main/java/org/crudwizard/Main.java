5cf2e03cef7e61f4821959a47ced993dbf2368f6
Nenad Samardzic <samardzicnenad@gmail.com>
2016-01-15 20:11:38 -0500

    AM/NS: Added controllers actions

--- src/main/java/org/crudwizard/Main.java
+++ src/main/java/org/crudwizard/Main.java
@@ -1,10 +1,10 @@
 package org.crudwizard;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.codahale.metrics.annotation.Timed;
 import com.squareup.javapoet.*;
-import org.apache.commons.lang3.text.WordUtils;
 
 import javax.lang.model.element.Modifier;
+import javax.ws.rs.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,24 +26,27 @@ public class Main {
         propertyList.add(new ClassProperty("again", String[].class));
         /////////////////////////////////////////////////////////
 
-        TypeSpec confClass = buildConfiguration(appName);
-        TypeSpec appClass = buildApp(appName, paramName, packageName);
+        TypeSpec cnfClass = buildConfiguration(appName);
+        TypeSpec appClass = buildApplication(appName, paramName, packageName);
         TypeSpec repClass = buildRepresentation(representationName, propertyList);
 
-        JavaFile javaFile = JavaFile.builder(packageName, confClass)
+        TypeSpec resClass = buildResource(representationName, packageName);
+        JavaFile javaFile = JavaFile.builder(packageName, resClass)
                 .build();
 
         javaFile.writeTo(System.out);
     }
 
     private static TypeSpec buildConfiguration(String appName) {
+
         return TypeSpec.classBuilder(appName + "Configuration")
                 .addModifiers(Modifier.PUBLIC)
                 .superclass(ClassName.get("io.dropwizard", "Configuration"))
                 .build();
     }
 
-    private static TypeSpec buildApp(String appName, String paramName, String packageName) {
+    private static TypeSpec buildApplication(String appName, String paramName, String packageName) {
+
         MethodSpec main = MethodSpec.methodBuilder("main")
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                 .returns(void.class)
@@ -95,5 +98,121 @@ public class Main {
         }
 
         return builder.build();
+    }
+
+    private static AnnotationSpec buildPathAnnotation(String endPoint) {
+        return AnnotationSpec.builder(Path.class)
+                .addMember("value", "$S", String.format("/%s", endPoint.toLowerCase()))
+                .build();
+    }
+
+    private static TypeSpec buildResource(String representationName, String packageName) {
+
+        ClassName className = ClassName.get(packageName, representationName);
+
+        TypeName listOfReps = ParameterizedTypeName.get(
+                ClassName.get("java.util", "List"),
+                className
+        );
+
+        AnnotationSpec rootPathAnnotation = buildPathAnnotation(representationName);
+
+        ClassName mediaType = ClassName.get("javax.ws.rs.core", "MediaType");
+        AnnotationSpec produceAnnotation = AnnotationSpec.builder(Produces.class)
+                .addMember("value", "$T", mediaType.nestedClass("APPLICATION_JSON"))
+                .build();
+
+        MethodSpec index = MethodSpec.methodBuilder("index")
+                .addAnnotation(GET.class)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(listOfReps)
+                .addStatement("//return Tweet.findAll()")
+                .addStatement("return null")
+                .build();
+
+        AnnotationSpec newPathAnnotation = buildPathAnnotation("new");
+        MethodSpec newObject = MethodSpec.methodBuilder("newObject")
+                .addAnnotation(GET.class)
+                .addAnnotation(newPathAnnotation)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(String.class)
+                .addStatement("//return HTML form for creating a new tweet")
+                .addStatement("return null")
+                .build();
+
+        MethodSpec create = MethodSpec.methodBuilder("create")
+                .addAnnotation(POST.class)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(void.class)
+                .addStatement("//save the tweet in the database")
+                .build();
+
+        ParameterSpec idParameter = ParameterSpec.builder(Integer.class, "id")
+                .addAnnotation(AnnotationSpec.builder(PathParam.class).addMember("value", "$S", "id").build())
+                .build();
+
+        AnnotationSpec showPathAnnotation = buildPathAnnotation("{id}");
+        MethodSpec show = MethodSpec.methodBuilder("show")
+                .addAnnotation(GET.class)
+                .addAnnotation(showPathAnnotation)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(idParameter)
+                .returns(className)
+                .addStatement("//return Tweet.findById(id)")
+                .addStatement("return null")
+                .build();
+
+        AnnotationSpec editPathAnnotation = buildPathAnnotation("{id}/edit");
+        MethodSpec edit = MethodSpec.methodBuilder("edit")
+                .addAnnotation(GET.class)
+                .addAnnotation(editPathAnnotation)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(idParameter)
+                .returns(String.class)
+                .addStatement("//return HTML form for editing the message")
+                .addStatement("return null")
+                .build();
+
+        AnnotationSpec putPathAnnotation = buildPathAnnotation("{id}");
+        MethodSpec put = MethodSpec.methodBuilder("put")
+                .addAnnotation(PUT.class)
+                .addAnnotation(putPathAnnotation)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(idParameter)
+                .returns(className)
+                .addStatement("//update in database and return the tweet")
+                .addStatement("return null")
+                .build();
+
+        AnnotationSpec deletePathAnnotation = buildPathAnnotation("{id}");
+        MethodSpec delete = MethodSpec.methodBuilder("destroy")
+                .addAnnotation(DELETE.class)
+                .addAnnotation(deletePathAnnotation)
+                .addAnnotation(Timed.class)
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(idParameter)
+                .returns(void.class)
+                .addStatement("//delete the tweet in database")
+                .addStatement("return null")
+                .build();
+
+        return TypeSpec.classBuilder(representationName + "Resource")
+                .addAnnotation(rootPathAnnotation)
+                .addAnnotation(produceAnnotation)
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(index)
+                .addMethod(newObject)
+                .addMethod(create)
+                .addMethod(show)
+                .addMethod(edit)
+                .addMethod(put)
+                .addMethod(delete)
+                .build();
     }
 }
